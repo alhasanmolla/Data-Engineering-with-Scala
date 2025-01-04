e7cef7bdb65e3a38dddd43c5aca6237b09aa3996
alhasanmolla <alhasannur99@gmail.com>
2025-01-04 16:38:59 +0600

    Add files via upload
    DBT tools

---
+++ dbt-mysql/sales_data_transformation/target/run/sales_data_transformation/models/sales_summary.sql
@@ -0,0 +1,15 @@
+
+  create view `ecommerce`.`sales_summary__dbt_tmp`
+    
+    
+  as (
+    -- models/sales_summary.sql
+
+SELECT
+    product_id,
+    SUM(sale_amount) AS total_sales
+FROM
+    ecommerce.sales  -- Ensure this table exists
+GROUP BY
+    product_id;
+  );
